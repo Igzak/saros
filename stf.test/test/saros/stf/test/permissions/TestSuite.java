318c77b61050ea4aed14462cd1a629a2a676e3d8
Tobias Bouschen <tobias.bouschen@googlemail.com>
2020-09-08 00:40:53 +0200

    [STF] Deletes WriteAccessChangeAndImmediateWriteTest
    Deletes the test in WriteAccessChangeAndImmediateWriteTest as it fails
    on the build server without an apparent reason. I could not find out why
    this happens by looking at the logs or the screenshots of the failed
    runs. Furthermore, the issue could not be reproduced when running the
    test on its own using Eclipse (on Linux or Windows) or the docker image.
    
    The test did not provide a lot of value to begin with, so loosing it
    should not hurt too much.

--- stf.test/test/saros/stf/test/permissions/TestSuite.java
+++ stf.test/test/saros/stf/test/permissions/TestSuite.java
@@ -4,10 +4,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({
-  AllParticipantsFollowUserWithWriteAccessTest.class,
-  WriteAccessChangeAndImmediateWriteTest.class
-})
+@Suite.SuiteClasses({AllParticipantsFollowUserWithWriteAccessTest.class})
 public class TestSuite {
   // the class remains completely empty,
   // being used only as a holder for the above annotations
